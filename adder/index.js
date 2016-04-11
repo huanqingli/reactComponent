bb63f78485e49c667711a02a899e681539191dfc
Huanqing Li <hli122@uottawa.ca>
2016-04-11 22:05:51 +0800

    adder

--- adder/index.js
+++ adder/index.js
@@ -5,33 +5,37 @@ var AdderOne = React.createClass({
         return { n: 0 };
     },
     click: function click() {
-        this.setState({ n: this.state.n + this.props.x });
+        this.setState({ n: this.state.n + 1 });
     },
     render: function render() {
         return React.createElement(
             "div",
-            { onClick: this.click },
+            { onClick: this.click, className: "item1" },
             "你点击了",
             this.state.n,
             "次"
         );
     }
 });
-ReactDOM.render(React.createElement(AdderOne, { x: 1 }), document.getElementById("add1"));
-"use strict";
-
+ReactDOM.render(React.createElement(AdderOne, null), document.getElementById("add1"));
 var AddX = React.createClass({
     displayName: "AddX",
 
+    getInitialState: function getInitialState() {
+        return { n: 0 };
+    },
     changeValue: function changeValue() {
         this.props.callBack(this.props.x);
+        this.setState({ n: this.state.n + 1 });
     },
     render: function render() {
         return React.createElement(
             "div",
-            { onClick: this.changeValue },
+            { onClick: this.changeValue, className: "item2_1_1" },
             this.props.x,
-            "元"
+            "元",
+            this.state.n,
+            "件"
         );
     }
 });
@@ -39,25 +43,31 @@ var Adder = React.createClass({
     displayName: "Adder",
 
     getInitialState: function getInitialState() {
-        return { n: 0 };
+        return { n: 0, u: 0 };
     },
     click: function click(x) {
-        this.setState({ n: this.state.n + x });
+        this.setState({ n: this.state.n + x,
+            u: this.state.u + 1 });
     },
     render: function render() {
         return React.createElement(
             "div",
-            null,
-            React.createElement(AddX, { x: 1, callBack: this.click }),
-            React.createElement(AddX, { x: 3, callBack: this.click }),
-            React.createElement(AddX, { x: 5, callBack: this.click }),
-            React.createElement(AddX, { x: 10, callBack: this.click }),
+            { className: "item2" },
             React.createElement(
                 "div",
-                { id: "show" },
+                { className: "item2_1" },
+                React.createElement(AddX, { x: 1, callBack: this.click }),
+                React.createElement(AddX, { x: 3, callBack: this.click }),
+                React.createElement(AddX, { x: 5, callBack: this.click }),
+                React.createElement(AddX, { x: 10, callBack: this.click })
+            ),
+            React.createElement(
+                "div",
+                { className: "item2_2" },
                 "共计",
                 this.state.n,
-                "元"
+                "元",
+                this.state.u
             )
         );
     }
